92b27c8c67b951cbe890b974117f5ff44b944422
1475351569@qq.com <1475351569@qq.com>
2024-06-30 17:54:18 +0800

    fix

--- example/src/main/java/org/fastlight/fastaop/example/handler/LogAccess.java
+++ example/src/main/java/org/fastlight/fastaop/example/handler/LogAccess.java
@@ -1,13 +1,17 @@
 package org.fastlight.fastaop.example.handler;
 
+import org.fastlight.aop.annotation.FastAspect;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 /**
  * 打印方法的调用时间
+ *
  * @author ychost@outlook.com
  * @date 2021-04-10
  */
 @Target(ElementType.METHOD)
+@FastAspect
 public @interface LogAccess {
 }
